7301e9ed52b5a2a83ea115ed18dab7e94cccd2b8
argalarza <andyricardog234@gmail.com>
2025-03-07 10:27:56 -0500

    Actualización del repo

--- services/login/controllers/logincontroller.js
+++ services/login/controllers/logincontroller.js
@@ -1,36 +1,54 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const axios = require('axios');
 const { getUserByUsername } = require('../models/user'); // Asegúrate de que la ruta sea correcta
 
 const loginUser = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, captcha } = req.body;
+
+  // Verificar el CAPTCHA utilizando la clave secreta
+  const secretKey = '6LeXYOwqAAAAAGezd7O1xeTe45n0TGiUcWngeqqF'; // Clave secreta obtenida de Google
 
   try {
+    // Verificar el reCAPTCHA con la API de Google
+    const captchaResponse = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify`,
+      null,
+      {
+        params: {
+          secret: secretKey,
+          response: captcha, // El valor que recibimos desde el front-end
+        },
+      }
+    );
+
+    if (!captchaResponse.data.success) {
+      return res.status(400).json({ message: 'CAPTCHA no válido' });
+    }
+
     // Buscar al usuario por su nombre de usuario
     const user = await getUserByUsername(username);
-
     if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Credenciales incorrectas' });
     }
 
     // Comparar la contraseña ingresada con la almacenada en la base de datos
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Credenciales incorrectas' });
     }
 
     // Generar el token JWT
     const token = jwt.sign(
       { id: user.id, username: user.username },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: '5h' }
     );
 
     res.json({ token });
-
   } catch (error) {
     console.error('Login error:', error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Error en el servidor' });
   }
 };
 
